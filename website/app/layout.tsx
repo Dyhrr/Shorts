c98f9c7aa9a547d0af2e151599d8875cc80f590b
Dyhr <nicklas.d.jespersen@gmail.com>
2025-06-18 13:42:28 +0200

    Add promotional Next.js website

---
+++ website/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import type { ReactNode } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'ShortSplit',
+  description: 'Effortless vertical video stacking'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
